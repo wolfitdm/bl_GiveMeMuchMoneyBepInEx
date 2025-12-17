ee7058aa7698a00580010b8996f63cfb1ade7586
wolfitdm <wolfitdm@gmail.com>
2025-12-17 07:43:47 +0100

    update the correct bepinex plugin

---
+++ obj/Debug/net40/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace PleasurePartyUnlimitedAll
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "bl_GiveMeMuchMoneyBepInEx";
+        public const string PLUGIN_NAME = "You have unlimited all!";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
